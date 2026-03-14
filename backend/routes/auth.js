be37887478a73cde8216418e2161557b948993aa
karampudijanardhan <janardhankarampudi@gmail.com>
2026-03-14 17:10:56 +0530

    fix router error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,30 +1,54 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hash
+    });
+
+    res.json({ message: "User Registered" });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
 
-if (!user) {
-  return res.status(400).json({ message: "User not found" });
-}
+  const valid = await bcrypt.compare(password, user.password);
 
-const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    return res.status(400).json({ message: "Wrong password" });
+  }
 
-if (!valid) {
-  return res.status(400).json({ message: "Wrong password" });
-}
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET
+  );
 
-const token = jwt.sign(
-  { id: user._id },
-  process.env.JWT_SECRET
-);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+  });
 
-res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none"
+  res.json({ message: "Login success" });
 });
 
-res.json({ message: "Login success" });
-
-});
+module.exports = router;
